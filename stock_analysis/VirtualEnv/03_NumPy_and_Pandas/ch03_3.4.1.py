0f5f03f204d63f0ddaf200ea038fc39f93a3011a
COCO <hong.git@yahoo.com>
2024-10-27 00:06:50 +0900

    그래프 출력

--- stock_analysis/VirtualEnv/03_NumPy_and_Pandas/ch03_3.4.1.py
+++ stock_analysis/VirtualEnv/03_NumPy_and_Pandas/ch03_3.4.1.py
@@ -9,8 +9,22 @@ end_date = datetime(2021,12,31)
 
 # yf.download을 사용하여 데이터를 불러오기
 sec = yf.download("005930.KS", start="2022-01-01", end="2022-12-31")
-print(sec)
+#print(sec)
+sec_dpc = (sec['Close']-sec['Close'].shift(1)) / sec['Close'].shift(1) * 100
+sec_dpc.iloc[0] = 0 # 일간 변동률의 첫 번째 값인 NaN을 0으로 변경한다.
+sec_dpc_cp = ((100+sec_dpc)/100).cumprod()*100-100 # 일간 변동률 누적곱 계산
 
+msft = yf.download("AAPL", start="2022-01-01", end="2022-12-31")
+#print(msft)
+msft_dpc = (msft['Close'] / msft['Close'].shift(1) -1) * 100
+msft_dpc.iloc[0] = 0
+msft_dpc_cp = ((100+msft_dpc)/100).cumprod()*100-100
 
-data = yf.download("AAPL", start="2022-01-01", end="2022-12-31")
-print(data)
+import matplotlib.pyplot as plt
+plt.plot(sec.index, sec_dpc_cp, 'b', label='Samsung Electronics')
+plt.plot(msft.index, msft_dpc_cp, 'r--', label='Microsoft')
+plt.ylabel('Change %') 
+plt.grid(True)
+plt.legend(loc='best')
+plt.show()
+
