02e064f05f1e14ad1f99f999d5830bb974743f56
COCO <hong.git@yahoo.com>
2024-10-27 00:04:49 +0900

    애플 주가 구하기

--- stock_analysis/VirtualEnv/03_NumPy_and_Pandas/ch03_3.4.1.py
+++ stock_analysis/VirtualEnv/03_NumPy_and_Pandas/ch03_3.4.1.py
@@ -1,10 +1,16 @@
 from datetime import datetime 
-from pandas_datareader import data as pdr
+import pandas_datareader as pdr
 import yfinance as yf
+
+print('pandas_datareader version: ' + pdr.__version__)
 
 start_date = datetime(2021,1,1) 
 end_date = datetime(2021,12,31)
 
 # yf.download을 사용하여 데이터를 불러오기
+sec = yf.download("005930.KS", start="2022-01-01", end="2022-12-31")
+print(sec)
+
+
 data = yf.download("AAPL", start="2022-01-01", end="2022-12-31")
 print(data)
