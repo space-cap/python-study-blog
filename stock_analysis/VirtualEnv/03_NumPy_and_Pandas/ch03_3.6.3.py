cc51acf63b54eba410c55bac7c493c0da76037b8
COCO <hong.git@yahoo.com>
2024-10-27 01:06:07 +0900

    오류가 남

---
+++ stock_analysis/VirtualEnv/03_NumPy_and_Pandas/ch03_3.6.3.py
@@ -0,0 +1,25 @@
+import pandas_datareader as pdr
+import yfinance as yf
+import pandas as pd
+from datetime import datetime
+
+# KOSPI와 다우존스 지수화 비교
+# 날짜 설정
+start_date = datetime(2024,1,1) 
+end_date = datetime(2024,12,31)
+
+# yf.download을 사용하여 데이터를 불러오기
+dow = yf.download("^DJI", start_date, end_date)
+kospi = yf.download("^KS11", start_date, end_date)
+
+
+df = pd.DataFrame({'DOW': dow['Close'], 'KOSPI': kospi['Close']})
+df = df.fillna(method='bfill')
+df = df.fillna(method='ffill')
+
+import matplotlib.pyplot as plt
+plt.figure(figsize=(7, 7))
+plt.scatter(df['DOW'], df['KOSPI'], marker='.')
+plt.xlabel('Dow Jones Industrial Average')
+plt.ylabel('KOSPI')
+plt.show()
