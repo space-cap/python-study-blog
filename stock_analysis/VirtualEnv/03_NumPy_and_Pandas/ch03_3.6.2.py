2ec0f52b7f8cd497464d8f8cf1c53d463b5cfd8a
COCO <hong.git@yahoo.com>
2024-10-27 00:52:28 +0900

    날짜 수정

--- stock_analysis/VirtualEnv/03_NumPy_and_Pandas/ch03_3.6.2.py
+++ stock_analysis/VirtualEnv/03_NumPy_and_Pandas/ch03_3.6.2.py
@@ -4,7 +4,7 @@ import pandas as pd
 from datetime import datetime
 
 # KOSPI와 다우존스 지수화 비교
-
+# 날짜 설정
 start_date = datetime(2024,1,1) 
 end_date = datetime(2024,12,31)
 
@@ -12,8 +12,9 @@ end_date = datetime(2024,12,31)
 dow = yf.download("^DJI", start_date, end_date)
 kospi = yf.download("^KS11", start_date, end_date)
 
-d = (dow.Close / dow.Close.loc['2024-01-01']) * 100
-k = (kospi.Close / kospi.Close.loc['2024-01-01']) * 100
+# 기준일 대비 지수 계산
+d = (dow.Close / dow.Close.loc['2024-01-02']) * 100
+k = (kospi.Close / kospi.Close.loc['2024-01-02']) * 100
 
 import matplotlib.pyplot as plt
 plt.figure(figsize=(9, 5))
