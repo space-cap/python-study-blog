bd23cf14b461465e7ef474f8f89e791b3a91f74d
COCO <hong.git@yahoo.com>
2024-10-27 15:46:25 +0900

    맨 뒤 페이지 숫자 구하기

---
+++ stock_analysis/VirtualEnv/04_Web_Scraping/ch04_4.4.3.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://finance.naver.com/item/sise_day.nhn?code=068270&page=1'
+html = requests.get(url, headers={'User-agent': 'Mozilla/5.0'}).text
+bs = BeautifulSoup(html, 'lxml')
+pgrr = bs.find('td', class_='pgRR')
+s = str(pgrr.a['href']).split('=')
+last_page = s[-1]
+
+print(last_page)
+
