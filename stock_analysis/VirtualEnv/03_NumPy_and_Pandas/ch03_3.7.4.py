e3646810fb4518d6ce5361e1c031183d1fb36ea7
COCO <hong.git@yahoo.com>
2024-10-27 15:29:52 +0900

    코드 수정

--- stock_analysis/VirtualEnv/03_NumPy_and_Pandas/ch03_3.7.4.py
+++ stock_analysis/VirtualEnv/03_NumPy_and_Pandas/ch03_3.7.4.py
@@ -14,22 +14,9 @@ end_date = datetime(2024,12,31)
 dow = yf.download("^DJI", start_date, end_date)
 kospi = yf.download("^KS11", start_date, end_date)
 
-# 인덱스를 맞춘 후 DataFrame 생성
-df = pd.concat([dow['Close'], kospi['Close']], axis=1, keys=['DOW', 'KOSPI'])
+# 데이터를 병합하여 인덱스가 일치하도록 DataFrame 생성
+df = pd.concat([dow['Close'], kospi['Close']], axis=1, keys=['X', 'Y'])
 df = df.fillna(method='bfill')
 df = df.fillna(method='ffill')
 
-# NaN 값을 제거한 후 선형 회귀 수행
-df = df.dropna()
-regr = stats.linregress(df['DOW'], df['KOSPI'])
-regr_line = f'Y = {regr.slope:2f} X + {regr.intercept:2f}'
-
-# 그래프 생성
-plt.figure(figsize=(7, 7))
-plt.plot(df['DOW'], df['KOSPI'], '.') 
-plt.plot(df['DOW'], regr.slope * df['DOW'] + regr.intercept, 'r')
-plt.legend(['DOW x KOSPI', regr_line])
-plt.title(f'DOW x KOSPI (R = {regr.rvalue:2f})')
-plt.xlabel('Dow Jones Industrial Average')
-plt.ylabel('KOSPI')
-plt.show()
+print(df.head())  # 데이터 확인
