b4bdd0e0ec4197fa162d50756bde0b62c151cadb
COCO <hong.git@yahoo.com>
2024-10-27 21:49:17 +0900

    열 이름 확인

--- stock_analysis/VirtualEnv/05_Stock_Price_API/ch05_01_YahooFinance_SEC.py
+++ stock_analysis/VirtualEnv/05_Stock_Price_API/ch05_01_YahooFinance_SEC.py
@@ -12,23 +12,6 @@ end_date = datetime(2019,12,31)
 # yf.download을 사용하여 데이터를 불러오기
 df = yf.download("005930.KS", start_date, end_date)
 
-# NaN 값 제거
-df = df.dropna(subset=['Close', 'Adj Close', 'Volume'])
-
-# 그래프 설정
-plt.figure(figsize=(9, 6))
-
-# 종가와 수정 종가 라인 차트
-plt.subplot(2, 1, 1) 
-plt.title('Samsung Electronics (Yahoo Finance)')
-plt.plot(df.index, df['Close'], 'c', label='Close')
-plt.plot(df.index, df['Adj Close'], 'b--', label='Adj Close')
-plt.legend(loc='best')
-
-# 거래량 바 차트
-plt.subplot(2, 1, 2)
-plt.bar(df.index, df['Volume'], color='g', label='Volume')
-plt.legend(loc='best')
-
-plt.show()
+# 실제 열 이름 확인
+print("Columns in DataFrame:", df.columns)
 
