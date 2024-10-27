140c6b8673cba1636eb2d9a6996037ccd7e3345d
COCO <hong.git@yahoo.com>
2024-10-27 22:04:54 +0900

    열 이름 수정

--- stock_analysis/VirtualEnv/05_Stock_Price_API/ch05_01_YahooFinance_SEC.py
+++ stock_analysis/VirtualEnv/05_Stock_Price_API/ch05_01_YahooFinance_SEC.py
@@ -7,7 +7,7 @@ from scipy import stats
 from datetime import datetime
 
 start_date = datetime(2017,1,1) 
-end_date = datetime(2019,12,31)
+end_date = datetime(2017,1,31)
 
 # yf.download을 사용하여 데이터를 불러오기
 df = yf.download("005930.KS", start_date, end_date)
@@ -15,12 +15,9 @@ df = yf.download("005930.KS", start_date, end_date)
 # 실제 열 이름 확인
 print("Columns in DataFrame:", df.columns)
 
-df = df.rename(columns={('Adj Close', '005930.KS'):'Adj Close', 
-                        ('Close', '005930.KS'):'Open', 
-                        ('High', '005930.KS'):'High', 
-                        ('Low', '005930.KS'):'Low', 
-                        ('Open', '005930.KS'):'Close', 
-                        ('Volume', '005930.KS'):'Volume'})
+# 예시: 첫 번째 레벨 이름을 특정 열로 바꾸기
+df.columns = df.columns.set_levels(['Adj Close', 'Open', 'High', 'Low', 'Close', 'Volume'], level=1)
+
 
 
 print("Columns in DataFrame:", df.columns)
