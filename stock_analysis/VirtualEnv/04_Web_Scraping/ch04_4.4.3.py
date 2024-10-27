ca945fd2b7d6ca2eacb0b7521d0af06afbbe5a5e
COCO <hong.git@yahoo.com>
2024-10-27 15:58:51 +0900

    오류

--- stock_analysis/VirtualEnv/04_Web_Scraping/ch04_4.4.3.py
+++ stock_analysis/VirtualEnv/04_Web_Scraping/ch04_4.4.3.py
@@ -1,12 +1,35 @@
 import requests
+import pandas as pd
+import time
 from bs4 import BeautifulSoup
+
+# 맨 뒤 페이지 숫자 구하기
 
 url = 'https://finance.naver.com/item/sise_day.nhn?code=068270&page=1'
 html = requests.get(url, headers={'User-agent': 'Mozilla/5.0'}).text
 bs = BeautifulSoup(html, 'lxml')
 pgrr = bs.find('td', class_='pgRR')
+print(pgrr.a['href'])
+
 s = str(pgrr.a['href']).split('=')
+print(s)
+
 last_page = s[-1]
 
 print(last_page)
+
+
+last_page = 2 # 테스트를 위해서 2로 수정
+
+# 4.4.4 전체 페이지 읽어오기
+df = pd.DataFrame()
+sise_url = 'https://finance.naver.com/item/sise_day.nhn?code=068270'  
+for page in range(1, int(last_page)+1):
+    url = '{}&page={}'.format(sise_url, page)  
+    html = requests.get(url, headers={'User-agent': 'Mozilla/5.0'}).text
+    df = df.append(pd.read_html(html, header=0)[0])
+    time.sleep(2)  # 2초 동안 멈춤
+
+print(df)
+
 
