cddbc0a0d3c4247b44b017031f614b4f7f794208
COCO <hong.git@yahoo.com>
2024-10-27 15:35:33 +0900

    오류 수정

--- stock_analysis/VirtualEnv/03_NumPy_and_Pandas/ch03_3.7.4.py
+++ stock_analysis/VirtualEnv/03_NumPy_and_Pandas/ch03_3.7.4.py
@@ -21,16 +21,9 @@ df = df.fillna(method='ffill')
 
 # print(df.head())  # 데이터 확인
 
-# 선형 회귀 수행
-regr = stats.linregress(df.X, df.Y)
-regr_line = f'Y = {regr.slope:2f}  X + {regr.intercept:2f}'
+# NaN 값 제거
+df = df.dropna()
 
-(Py3810_32) C:\workdir\github-space-cap\python-study-blog\stock_analysis\VirtualEnv\03_NumPy_and_Pandas>python ch03_3.7.4.py
-[*********************100%***********************]  1 of 1 completed
-[*********************100%***********************]  1 of 1 completed
-Traceback (most recent call last):
-  File "ch03_3.7.4.py", line 25, in <module>
-    regr = stats.linregress(df.X, df.Y)
-  File "C:\workdir\github-space-cap\python-study-blog\stock_analysis\VirtualEnv\Py3810_32\lib\site-packages\scipy\stats\_stats_mstats_common.py", line 166, in linregress
-    ssxm, ssxym, _, ssym = np.cov(x, y, bias=1).flat
-ValueError: too many values to unpack (expected 4)
+# 데이터 유형 및 결측치 확인
+print(df[['X', 'Y']].info())  # 결측치가 없고 float64 타입인지 확인
+
