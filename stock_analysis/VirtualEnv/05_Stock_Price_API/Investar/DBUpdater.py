5e58ffd04369f378013fefbf14cc9018aa1688ca
space-cap <hong.git@yahoo.com>
2024-10-30 16:01:43 +0900

    코드 정리

--- stock_analysis/VirtualEnv/05_Stock_Price_API/Investar/DBUpdater.py
+++ stock_analysis/VirtualEnv/05_Stock_Price_API/Investar/DBUpdater.py
@@ -45,9 +45,11 @@ class DBUpdater:
         self.conn.commit()
         self.codes = dict()
     
+    
     def __del__(self):
         """소멸자: MariaDB 연결 해제"""
         self.conn.close()
+
 
     def read_krx_code(self):
         """KRX로부터 상장기업 목록 파일을 읽어와서 데이터프레임으로 반환"""
@@ -58,6 +60,7 @@ class DBUpdater:
         krx = krx.rename(columns={'종목코드': 'code', '회사명': 'company'})
         krx.code = krx.code.map('{:06d}'.format)
         return krx
+
 
     def update_comp_info(self):
         """종목코드를 company_info 테이블에 업데이트 한 후 딕셔너리에 저장"""
@@ -86,6 +89,7 @@ class DBUpdater:
                         f"VALUES ({code}, {company}, {today})")
                 self.conn.commit()
                 print('')         
+
 
     def read_naver(self, code, company, pages_to_fetch):
         """네이버에서 주식 시세를 읽어서 데이터프레임으로 반환"""
@@ -130,19 +134,27 @@ class DBUpdater:
             # NaN 값 제거
             df = df.dropna()  # NaN이 포함된 행을 제거합니다. # 값이 빠진 행을 제거한다.
 
-            print(df)
+            # print(df)
 
-            return None
+            df = df.rename(columns={'날짜':'date','종가':'close','전일비':'diff'
+                ,'시가':'open','고가':'high','저가':'low','거래량':'volume'})
+            
+            df['date'] = df['date'].replace('.', '-')
+
+            df[['close', 'diff', 'open', 'high', 'low', 'volume']] = df[['close',
+                'diff', 'open', 'high', 'low', 'volume']].astype(int)
+            df = df[['date', 'open', 'high', 'low', 'close', 'diff', 'volume']]
+
         except Exception as e:
             print('Exception occurred:', str(e))
             return None
 
-        return None
-
+        return df
 
 
     def replace_into_db(self, df, num, code, company):
         """네이버에서 읽어온 주식 시세를 DB에 REPLACE"""
+
 
     def execute_daily(self):
         """실행 즉시 및 매일 오후 다섯시에 daily_price 테이블 업데이트"""
