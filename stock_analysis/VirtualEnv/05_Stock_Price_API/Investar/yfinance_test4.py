f255df95058ca0904af502c247439d0a4e3874d4
COCO <hong.git@yahoo.com>
2024-11-03 00:48:17 +0900

    Update yfinance_test4.py

--- stock_analysis/VirtualEnv/05_Stock_Price_API/Investar/yfinance_test4.py
+++ stock_analysis/VirtualEnv/05_Stock_Price_API/Investar/yfinance_test4.py
@@ -25,6 +25,16 @@ host = 'localhost'           # MariaDB 호스트 (예: 'localhost')
 port = 3306                  # 포트 번호 (기본값: 3306)
 database = 'investar'   # 데이터베이스 이름
 
+# MariaDB 연결 설정
+conn = pymysql.connect(
+    host='localhost',
+    user=db_user,
+    password=db_password,
+    database='investar',
+    charset='utf8',
+    cursorclass=pymysql.cursors.DictCursor
+)
+
 # SQLAlchemy 연결 문자열을 생성
 engine = create_engine(f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}")
 
@@ -39,29 +49,38 @@ print(company_info)
 dart = dart_fss.auth.set_api_key(api_key=dart_api_key)  # DART API 키 설정
 corp_list = dart_fss.corp.get_corp_list()
 
+today = datetime.today().strftime('%Y-%m-%d')
+
 # 4. 모든 code에 대해 데이터를 처리
-for index, row in company_info.iterrows():
-    code = row['code']
-    company = row['company']
+with conn.curs() as curs:
+    for index, row in company_info.iterrows():
+        code = row['code']
+        company = row['company']
 
-    # 종목 코드로 기업 검색 (종목 코드: '005930')
-    corp = corp_list.find_by_stock_code(code)
+        # 종목 코드로 기업 검색 (종목 코드: '005930')
+        corp = corp_list.find_by_stock_code(code)
 
-    # 기업 분류 정보 확인
-    # market : str or list of str, optional
-    # 'Y': 코스피, 'K': 코스닥, 'N': 코넥스, 'E': 기타
-    corp_classification = corp.info['corp_cls']
+        # 기업 분류 정보 확인
+        # market : str or list of str, optional
+        # 'Y': 코스피, 'K': 코스닥, 'N': 코넥스, 'E': 기타
+        corp_classification = corp.info['corp_cls']
 
-    if corp_classification == 'Y':
-        code_suffix = '.KS'
-    elif corp_classification == 'K':
-        code_suffix = '.KQ'
-    else:
-        print(f"Warning: {company}/{code}/{corp_classification} 의 시장 구분을 찾을 수 없습니다.")
-        continue  # 다음 코드로 넘어감
+        if corp_classification == 'Y':
+            code_suffix = '.KS'
+        elif corp_classification == 'K':
+            code_suffix = '.KQ'
+        else:
+            print(f"Warning: {company}/{code}/{corp_classification} 의 시장 구분을 찾을 수 없습니다.")
+            # continue  # 다음 코드로 넘어감
 
-    # 종목 코드 변환
-    ticker = code + code_suffix
-    print(f"Processing Ticker: {ticker}")
+        # 종목 코드 변환
+        ticker = code + code_suffix
+        print(f"Processing Ticker: {ticker}")
+
+        sql = f"REPLACE INTO company_info (code, company, last_update, market) "\
+            f" VALUES ('{code}', '{company}', '{today}', '{corp_classification}')"
+        curs.execute(sql)
+
+
 
 
