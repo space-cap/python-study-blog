66b71f631822c937c6691eec55b43d358386e4b5
COCO <hong.git@yahoo.com>
2024-11-03 00:22:03 +0900

    Update yfinance_test4.py

--- stock_analysis/VirtualEnv/05_Stock_Price_API/Investar/yfinance_test4.py
+++ stock_analysis/VirtualEnv/05_Stock_Price_API/Investar/yfinance_test4.py
@@ -5,6 +5,7 @@ import urllib3
 import certifi
 import requests
 import os
+import dart_fss
 from datetime import datetime
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
@@ -15,6 +16,7 @@ load_dotenv()
 # 환경 변수 가져오기
 db_password = os.getenv('DB_PASSWORD')
 db_user = os.getenv('DB_USER')
+dart_api_key = os.getenv('DART_FSS_KEY')
 
 # 1. SQLAlchemy를 사용하여 MariaDB 데이터베이스 연결
 user = db_user       # MariaDB 사용자 이름
@@ -33,5 +35,28 @@ company_info = pd.read_sql_query(query, engine)
 # 데이터가 제대로 읽혀졌는지 확인
 print(company_info)
 
+# 3. DART-FSS 라이브러리를 사용하여 KOSPI, KOSDAQ 구분
+dart = dart_fss.auth.set_api_key(api_key=dart_api_key)  # DART API 키 설정
+corp_list = dart_fss.corp.get_corp_list()
 
+# 4. 모든 code에 대해 데이터를 처리
+for index, row in company_info.iterrows():
+    code = row['code']
 
+    # 회사 코드로 시장 구분
+    corp = corp_list.find_by_corp_code(code)
+    market = corp.market
+
+    if market == 'KOSPI':
+        code_suffix = '.KS'
+    elif market == 'KOSDAQ':
+        code_suffix = '.KQ'
+    else:
+        print(f"Warning: {code}의 시장 구분을 찾을 수 없습니다.")
+        continue  # 다음 코드로 넘어감
+
+    # 종목 코드 변환
+    ticker = code + code_suffix
+    print(f"Processing Ticker: {ticker}")
+
+    
