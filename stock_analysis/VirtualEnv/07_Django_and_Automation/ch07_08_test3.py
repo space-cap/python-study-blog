4826f778232efaa112978be71910f7a21787ed2e
COCO <hong.git@yahoo.com>
2024-11-07 00:56:37 +0900

    test4

--- stock_analysis/VirtualEnv/07_Django_and_Automation/ch07_08_test3.py
+++ stock_analysis/VirtualEnv/07_Django_and_Automation/ch07_08_test3.py
@@ -6,9 +6,13 @@ import pandas as pd
 # yfinance를 사용하여 데이터 다운로드
 df = yf.download("036570.KS", start="2024-01-01", end="2024-11-05")
 df = df.reset_index()  # 인덱스를 초기화하여 'Date' 열을 열로 변환
-df['Date'] = df['Date'].dt.date  # 날짜에서 시간 정보를 제거하여 순수한 날짜 형식으로 변경
-# 'Date' 열을 datetime 객체로 변환
-df['Date'] = pd.to_datetime(df['Date'])
+# df['Date'] = df['Date'].dt.date  # 날짜에서 시간 정보를 제거하여 순수한 날짜 형식으로 변경
+# dataframe.index = pd.to_datetime(dataframe.index,format="%Y-%m-%d",utc=True)
+# df['Date'] = pd.to_datetime(df['Date'], format="%Y-%m-%d",utc=True)
+# df["Date"] = pd.to_datetime(df["Date"].astype(str) + " 00:00:00")
+df['Date'] = pd.to_datetime(df['Date'],format='%Y-%m-%d %H:%M:%S')
+# df = df.sort_values(by='Date')
+print(df['Date'].dtype)  # 이 값이 datetime64[ns]여야 합니다.
 
 df = df[['Date', 'Adj Close', 'Close', 'High', 'Low', 'Open', 'Volume']]
 df.columns = ['date', 'adj_close', 'close', 'high', 'low', 'open', 'volume']  # 컬럼 이름 변경
