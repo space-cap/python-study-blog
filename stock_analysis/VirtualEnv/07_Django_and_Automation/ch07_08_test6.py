5223c2df2aad481cda9b8d929c862ef53a15ce0e
COCO <hong.git@yahoo.com>
2024-11-07 01:24:59 +0900

    Update ch07_08_test6.py

--- stock_analysis/VirtualEnv/07_Django_and_Automation/ch07_08_test6.py
+++ stock_analysis/VirtualEnv/07_Django_and_Automation/ch07_08_test6.py
@@ -1,5 +1,6 @@
 import backtrader as bt
 import yfinance as yf
+import pandas as pd
 
 
 class MyStrategy(bt.Strategy):
@@ -25,8 +26,17 @@ cerebro.addstrategy(MyStrategy)
 # Add data feed to cerebro
 df = yf.download("036570.KS", start="2024-01-01", end="2024-11-05")
 print(df)
+
+df = df.reset_index()  # 인덱스를 초기화하여 'Date' 열을 열로 변환
 df.columns = ['date', 'adj_close', 'close', 'high', 'low', 'open', 'volume']  # 컬럼 이름 변경
 print(df)
+
+df['date'] = pd.to_datetime([x[:-9] for x in df['date'].squeeze().tolist()], dayfirst=True)
+df.set_index('date', inplace=True)
+print(df)
+
+
+
 
 data = bt.feeds.PandasData(dataname=df)
 
