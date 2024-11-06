95f53b8d32d3ce14fca5929745d63ff1054ae84e
COCO <hong.git@yahoo.com>
2024-11-07 00:14:38 +0900

    Update ch07_08_test3.py

--- stock_analysis/VirtualEnv/07_Django_and_Automation/ch07_08_test3.py
+++ stock_analysis/VirtualEnv/07_Django_and_Automation/ch07_08_test3.py
@@ -7,46 +7,8 @@ df = yf.download("036570.KS", start="2024-01-01", end="2024-11-05")
 df = df.reset_index()  # 인덱스를 초기화하여 'Date' 열을 열로 변환
 df['Date'] = df['Date'].dt.date  # 날짜에서 시간 정보를 제거하여 순수한 날짜 형식으로 변경
 
-# 열 이름을 Backtrader가 요구하는 형식에 맞게 변경
-df.rename(columns={
-    'Date': 'datetime',
-    'Open': 'open',
-    'High': 'high',
-    'Low': 'low',
-    'Close': 'close',
-    'Volume': 'volume'
-}, inplace=True)
+df = df[['Date', 'Adj Close', 'Close', 'High', 'Low', 'Open', 'Volume']]
+df.columns = ['date', 'adj_close', 'close', 'high', 'low', 'open', 'volume']  # 컬럼 이름 변경
 
-# 'openinterest' 열을 추가 (사용하지 않을 경우 0으로 설정)
-df['openinterest'] = 0
+print(df)
 
-# 데이터를 Backtrader 피드로 변환
-data_feed = bt.feeds.PandasData(dataname=df)
-
-# 전략 정의
-class RSIStrategy(bt.Strategy):
-    params = (('rsi_period', 14), ('rsi_overbought', 70), ('rsi_oversold', 30))
-
-    def __init__(self):
-        self.rsi = bt.indicators.RSI_SMA(self.data.close, period=self.params.rsi_period)
-
-    def next(self):
-        if self.position.size == 0:  # 포지션이 없을 때
-            if self.rsi < self.params.rsi_oversold:  # 매수 신호
-                self.buy()
-            elif self.rsi > self.params.rsi_overbought:  # 매도 신호
-                self.sell()
-        elif self.position.size > 0:  # 포지션이 있을 때
-            if self.rsi > self.params.rsi_overbought:  # RSI가 과매수 상태이면 매도
-                self.sell()
-        elif self.position.size < 0:  # 공매도 포지션일 때
-            if self.rsi < self.params.rsi_oversold:  # RSI가 과매도 상태이면 매수
-                self.buy()
-
-# Backtrader 설정
-cerebro = bt.Cerebro()
-cerebro.addstrategy(RSIStrategy)
-cerebro.adddata(data_feed)
-cerebro.broker.set_cash(10000000)  # 초기 자산 설정
-cerebro.run()
-cerebro.plot()
