9894468b140b7e2b56fcd3c4bf56a7818ba9f3a7
COCO <hong.git@yahoo.com>
2024-11-07 00:22:01 +0900

    Update ch07_08_test3.py

--- stock_analysis/VirtualEnv/07_Django_and_Automation/ch07_08_test3.py
+++ stock_analysis/VirtualEnv/07_Django_and_Automation/ch07_08_test3.py
@@ -1,16 +1,19 @@
 import datetime
 import backtrader as bt
 import yfinance as yf
+import pandas as pd
 
 # yfinance를 사용하여 데이터 다운로드
 df = yf.download("036570.KS", start="2024-01-01", end="2024-11-05")
 df = df.reset_index()  # 인덱스를 초기화하여 'Date' 열을 열로 변환
 df['Date'] = df['Date'].dt.date  # 날짜에서 시간 정보를 제거하여 순수한 날짜 형식으로 변경
+# 'Date' 열을 datetime 객체로 변환
+df['Date'] = pd.to_datetime(df['Date'])
 
 df = df[['Date', 'Adj Close', 'Close', 'High', 'Low', 'Open', 'Volume']]
 df.columns = ['date', 'adj_close', 'close', 'high', 'low', 'open', 'volume']  # 컬럼 이름 변경
 
-# print(df)
+print(df)
 
 # 데이터를 Backtrader 피드로 변환
 data_feed = bt.feeds.PandasData(dataname=df)
