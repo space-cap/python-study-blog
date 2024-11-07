0677a9a84a83baf5c9a6af2e3e4f1232869dacf5
space-cap <hong.git@yahoo.com>
2024-11-07 18:53:09 +0900

    Update ch06_02_PortfolioOptimization.py

--- stock_analysis/VirtualEnv/06_Trading_Strategy/ch06_02_PortfolioOptimization.py
+++ stock_analysis/VirtualEnv/06_Trading_Strategy/ch06_02_PortfolioOptimization.py
@@ -7,7 +7,7 @@ mk = Analyzer.MarketDB()
 stocks = ['삼성전자', 'SK하이닉스', '현대자동차', 'NAVER']
 df = pd.DataFrame()
 for s in stocks:
-    df[s] = mk.get_daily_price(s, '2016-01-04', '2018-04-27')['close']
+    df[s] = mk.get_daily_price(s, '2024-01-04', '2024-04-27')['close']
   
 daily_ret = df.pct_change() 
 annual_ret = daily_ret.mean() * 252
@@ -50,3 +50,4 @@ plt.title('Portfolio Optimization')
 plt.xlabel('Risk') 
 plt.ylabel('Expected Returns') 
 plt.show()
+
