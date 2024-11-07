42e585769697ce07511644bc8869a9082f453c2d
space-cap <hong.git@yahoo.com>
2024-11-07 17:02:27 +0900

    Update ch06_01_EfficientFrontier.py

--- stock_analysis/VirtualEnv/06_Trading_Strategy/ch06_01_EfficientFrontier.py
+++ stock_analysis/VirtualEnv/06_Trading_Strategy/ch06_01_EfficientFrontier.py
@@ -8,8 +8,39 @@ mk = Analyzer.MarketDB()
 stocks = ['삼성전자', 'SK하이닉스', '현대자동차', 'NAVER']
 df = pd.DataFrame()
 for s in stocks:
-    df[s] = mk.get_daily_price(s, '2016-01-04', '2018-04-27')['close']
+    df[s] = mk.get_daily_price(s, '2024-01-04', '2024-04-27')['close']
 
+# print(df)
 
+daily_ret = df.pct_change() 
+annual_ret = daily_ret.mean() * 252
+daily_cov = daily_ret.cov() 
+annual_cov = daily_cov * 252
+
+port_ret = [] 
+port_risk = [] 
+port_weights = [] 
+
+# print(daily_ret)
+# print(annual_ret)
+# print(daily_cov)
+# print(annual_cov)
+
+for _ in range(20000): 
+    weights = np.random.random(len(stocks)) 
+    weights /= np.sum(weights) 
+
+    returns = np.dot(weights, annual_ret) 
+    risk = np.sqrt(np.dot(weights.T, np.dot(annual_cov, weights))) 
+
+    port_ret.append(returns) 
+    port_risk.append(risk) 
+    port_weights.append(weights)
+
+portfolio = {'Returns': port_ret, 'Risk': port_risk} 
+for i, s in enumerate(stocks): 
+    portfolio[s] = [weight[i] for weight in port_weights] 
+df = pd.DataFrame(portfolio) 
+df = df[['Returns', 'Risk'] + [s for s in stocks]] 
 
 print(df)
