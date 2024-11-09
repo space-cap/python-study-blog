18058b8ac9b896bb18e517f53cf8fd28b2aea66c
COCO <hong.git@yahoo.com>
2024-11-09 23:58:58 +0900

    Update admin.py

--- djangoProject/mysite/pybo/admin.py
+++ djangoProject/mysite/pybo/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Question
 
 # Register your models here.
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+
+admin.site.register(Question, QuestionAdmin)
