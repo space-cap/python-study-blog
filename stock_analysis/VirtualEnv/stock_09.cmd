e71e99c280fbbbc1a463108ee0a0a36b115135a5
COCO <hong.git@yahoo.com>
2024-11-13 00:20:02 +0900

    3.8.10 64bit

---
+++ stock_analysis/VirtualEnv/stock_09.cmd
@@ -0,0 +1,3 @@
+@echo off
+cd C:\workdir\github-space-cap\python-study-blog\stock_analysis\VirtualEnv
+C:\workdir\github-space-cap\python-study-blog\stock_analysis\VirtualEnv\Py3810_64\Scripts\activate.bat
