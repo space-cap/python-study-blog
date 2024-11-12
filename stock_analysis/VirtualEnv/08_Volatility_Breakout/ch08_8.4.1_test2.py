5e03795b6bfde1533734bf436afde727ca904f2d
space-cap <hong.git@yahoo.com>
2024-11-12 14:50:50 +0900

    Create ch08_8.4.1_test2.py

---
+++ stock_analysis/VirtualEnv/08_Volatility_Breakout/ch08_8.4.1_test2.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+
+# Selenium 옵션 설정
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")  # 브라우저 창을 띄우지 않음
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+# chromedriver 경로 설정
+driver_path = "D:/chromedriver/chromedriver-130/chromedriver.exe"
+
+# Selenium WebDriver 초기화
+driver = webdriver.Chrome(executable_path=driver_path, options=options)
+
+try:
+    # 웹페이지 열기
+    url = "https://finance.naver.com/sise/etf.nhn"
+    driver.get(url)
+
+    # 페이지가 로드될 시간을 줌
+    time.sleep(3)
+
+    # 페이지 소스 가져오기
+    html = driver.page_source
+
+    # BeautifulSoup을 이용해 HTML 파싱
+    soup = BeautifulSoup(html, "html.parser")
+
+    # 예시: 페이지 제목 출력
+    print(soup.title.text)
+
+finally:
+    # 드라이버 종료
+    driver.quit()
