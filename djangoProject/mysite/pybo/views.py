5aa5d914c161f51b44f96841a6fe7f24fdbc4e6a
space-cap <hong.git@yahoo.com>
2024-11-18 17:49:54 +0900

    Update views.py

--- djangoProject/mysite/pybo/views.py
+++ djangoProject/mysite/pybo/views.py
@@ -26,7 +26,17 @@ def answer_create(request, question_id):
 
 
 def question_create(request):
-    form = QuestionForm()
-    return render(request, 'pybo/question_form.html', {'form': form})
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
+
 
 
