f59f3531dc7c09466c514924c45a4e864f50425e
space-cap <hong.git@yahoo.com>
2024-11-18 17:59:18 +0900

    view

--- djangoProject/mysite/pybo/forms.py
+++ djangoProject/mysite/pybo/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from pybo.models import Question
+from pybo.models import Question, Answer
 
 
 class QuestionForm(forms.ModelForm):
@@ -10,3 +10,15 @@ class QuestionForm(forms.ModelForm):
             'subject': '제목',
             'content': '내용',
         }  
+
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
+        labels = {
+            'content': '답변내용',
+        }
+
+
+        
