4f0940aaffb7f32aff0b7e82ead3b829c5d09c76
space-cap <hong.git@yahoo.com>
2024-11-18 17:25:23 +0900

    view

--- djangoProject/mysite/pybo/views.py
+++ djangoProject/mysite/pybo/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse  # 삭제
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from .models import Question
 
 
@@ -8,7 +9,15 @@ def index(request):
     context = {'question_list': question_list}
     return render(request, 'pybo/question_list.html', context)
 
+
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
+
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question.id)
+
