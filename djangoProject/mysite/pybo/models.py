7c178c4851a586d9c3888290eb5a89014a53abd1
space-cap <hong.git@yahoo.com>
2024-11-19 16:38:18 +0900

    Update models.py

--- djangoProject/mysite/pybo/models.py
+++ djangoProject/mysite/pybo/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Question(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
