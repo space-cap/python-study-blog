ac5fea618505efd8217db4f7582097c989e11ff5
space-cap <hong.git@yahoo.com>
2024-11-19 17:31:56 +0900

    models

--- djangoProject/mysite/pybo/models.py
+++ djangoProject/mysite/pybo/models.py
@@ -7,14 +7,17 @@ class Question(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.subject
 
 
 class Answer(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    
 
