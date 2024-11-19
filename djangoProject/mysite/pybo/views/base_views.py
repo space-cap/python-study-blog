8ac555b157b3e6bef360a02d4f53ec2664a9c5c2
space-cap <hong.git@yahoo.com>
2024-11-19 17:58:00 +0900

    views

---
+++ djangoProject/mysite/pybo/views/base_views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse  # 삭제
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from ..models import Question, Answer
+from ..forms import QuestionForm, AnswerForm
+from django.http import HttpResponseNotAllowed
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+
+def index(request):
+    page = request.GET.get('page', '1')  # 페이지
+    question_list = Question.objects.order_by('-create_date')
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj}
+    return render(request, 'pybo/question_list.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
+
+
