d44d41d7ab72e98ebf36dc24273b842c726508fe
space-cap <hong.git@yahoo.com>
2024-11-20 15:08:21 +0900

    Update question_views.py

--- djangoProject/mysite/pybo/views/question_views.py
+++ djangoProject/mysite/pybo/views/question_views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse  # 삭제
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .models import Question, Answer
-from .forms import QuestionForm, AnswerForm
+from ..models import Question, Answer
+from ..forms import QuestionForm, AnswerForm
 from django.http import HttpResponseNotAllowed
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
