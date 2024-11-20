dd63e2d4f2fe4ad4f657a91052d00c08978cb0d8
space-cap <hong.git@yahoo.com>
2024-11-20 15:27:13 +0900

    앵커

--- djangoProject/mysite/pybo/views/answer_views.py
+++ djangoProject/mysite/pybo/views/answer_views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse  # 삭제
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, resolve_url
 from django.utils import timezone
 from ..models import Question, Answer
 from ..forms import QuestionForm, AnswerForm
@@ -20,7 +20,8 @@ def answer_create(request, question_id):
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
-            return redirect('pybo:detail', question_id=question.id)
+            return redirect('{}#answer_{}'.format(
+                resolve_url('pybo:detail', question_id=question.id), answer.id))
     else:
         #return HttpResponseNotAllowed('Only POST is possible.')
         form = AnswerForm()
@@ -40,7 +41,8 @@ def answer_modify(request, answer_id):
             answer = form.save(commit=False)
             answer.modify_date = timezone.now()
             answer.save()
-            return redirect('pybo:detail', question_id=answer.question.id)
+            return redirect('{}#answer_{}'.format(
+                resolve_url('pybo:detail', question_id=answer.question.id), answer.id))
     else:
         form = AnswerForm(instance=answer)
     context = {'answer': answer, 'form': form}
@@ -64,5 +66,6 @@ def answer_vote(request, answer_id):
         messages.error(request, '본인이 작성한 글은 추천할수 없습니다')
     else:
         answer.voter.add(request.user)
-    return redirect('pybo:detail', question_id=answer.question.id)
+    return redirect('{}#answer_{}'.format(
+        resolve_url('pybo:detail', question_id=answer.question.id), answer.id))
 
