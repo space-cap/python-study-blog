a73c6a3f3d0015ee56bdbb0b0745d7ce07846e2a
COCO <hong.git@yahoo.com>
2024-11-21 11:51:45 +0900

    db 변경

--- djangoProject/mysite/config/settings.py
+++ djangoProject/mysite/config/settings.py
@@ -81,7 +81,7 @@ environ.Env.read_env(BASE_DIR / '.env')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
